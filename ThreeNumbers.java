465f229d32f069a75a1f2de873da663c4fe5c980
woong7164 <woong7164@gmail.com>
2020-07-10 01:17:15 +0900

    정리

--- ThreeNumbers.java
+++ ThreeNumbers.java
@@ -24,46 +24,8 @@ public class ThreeNumbers {
 	        if (a > c)  System.out.println(a);
 	         System.out.println(c);
 	    } 
-	    
+	    sc.close();
 	}
 }
-
-//	  public static void insortionSort(int a[]) {
-//	        int size = a.length;        
-//	        for(int i=1; i<size; i++) {
-//	            int temp = a[i];
-//	            int j = i;
-//	            while((j>0) && (a[j-1]>temp)) {
-//	                a[j] = a[j-1];
-//	                j--;
-//	            }
-//	            
-//	            a[j] = temp;
-//	            System.out.printf("\n삽입정렬 %d 단계 : ",i);
-//	            for(int v : a) {
-//	                System.out.printf("%3d ", v);
-//	            }            
-//	        }
-//	        System.out.println();
-//	    }
-
-
-
-//
-//30 50 20 10 50
-//30 20 50 10 50
-//30 20 10 50 50
-//30 20 10 50 50
-//
-//20 30 10 50 50 
-//20 10 30 50 50
-//20 10 30 50 50
-//
-//10 20 30 50 50
-//10 20 30 50 50
-//
-//10 20 30 50 50
-//
-//
 
 
