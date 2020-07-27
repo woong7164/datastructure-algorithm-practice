51fd4a4411a77dd32bc35d4a2b234d605f9337c8
woong7164 <woong7164@gmail.com>
2020-07-28 07:52:22 +0900

    잃어버린 수 찾기

---
+++ src/main/PermMissingElem.java
@@ -0,0 +1,19 @@
+// you can also use imports, for example:
+ import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int solution(int[] A) {
+        Arrays.sort(A);
+        
+        for(int i=0; i <A.length; i++){
+            if(A[i]!=i+1){
+                return i+1;
+            }
+        }
+        
+        return A.length+1;
+    }
+}
